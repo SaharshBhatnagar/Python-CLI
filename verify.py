941127a3b63485bbb6ac5ed4282a094a02e1f84b
Saharsh Bhatnagar <saharsh.cse@gmail.com>
2025-08-26 22:46:15 +0530

    applied Lunh's algorithm

---
+++ verify.py
@@ -0,0 +1,43 @@
+def is_valid_number():
+
+    card_number = input("Enter a card number you want to check: ")
+
+    card_number = card_number.replace("-", "")
+    card_number = card_number.replace(" ", "")
+
+    card_number = int(card_number)
+
+    check_digit = 0
+    drop_check_digit = True
+
+    # Store Check Digit
+    if drop_check_digit == True:
+        check_digit = card_number % 10
+        drop_check_digit = False
+
+    card_number = str(card_number)
+
+    card_number = card_number[:-1]
+    # Reverse Card Number after dropping Check digit
+    rev_card_number = card_number[::-1]
+
+    sum_odd_digits = 0
+    sum_even_digits = 0
+    for num  in rev_card_number[::2]:
+        num = int(num) * 2
+        if num > 9:
+            sum_odd_digits += (num - 9)
+        else:
+            sum_odd_digits += num
+
+    for num in rev_card_number[1::2]:
+          sum_even_digits += int(num)
+
+    total_sum = sum_odd_digits + sum_even_digits
+
+    if (10 -(total_sum % 10)) % 10  == check_digit:
+        return f"{card_number} is a Valid Card Number"
+    else: 
+        return f"{card_number} is Not A Valid Card Number"
+
+print(is_valid_number())
